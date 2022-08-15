e607fe7c98aa909582ef21178340bcd28c0c4a6b
AndrewFaugno <andrewfaugno825@gmail.com>
2022-08-14 22:49:58 -0400

    created mutation queries

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
